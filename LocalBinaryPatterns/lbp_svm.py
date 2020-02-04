0617e1bc978bc8a00aad252b41c39a9688a854d7
Alexandru Marcu <alexandru.marcu@airgapnetworks.com>
2020-02-05 00:13:47 +0200

    separate cvloo dataset

--- LocalBinaryPatterns/lbp_svm.py
+++ LocalBinaryPatterns/lbp_svm.py
@@ -26,7 +26,7 @@ data = []
 labels = []
 
 
-dirName = '../TrainingDataSet2'
+dirName = '../TrainingData-CVLOO'
 imagesPaths = os.listdir(dirName)
 imagesPaths.sort()
 
@@ -40,29 +40,23 @@ print('Training the algorithm based on our histograms')
 model = LinearSVC(C=100, max_iter=100000)
 model.fit(data, labels)
 
-dirName = "../TestDataSet2"
+dirName = "../TestData-CVLOO"
 imagesPaths = os.listdir(dirName)
 imagesPaths.sort()
 
 predictions = []
 for imagePath in imagesPaths:
     print(f'Verifying {imagePath}')
-    # load and cliasify the test images
     path = os.path.join(dirName,  imagePath)
     image = cv2.imread(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     hist = lbp.describe(gray)
-    # res = cosine_similarity(hist.reshape(1,-1), data[20].reshape(1,-1))
-    # print(res)
-    # exit()
     prediction = model.predict(hist.reshape(1, -1))
 
-    # save the prediction for each image
     predictions.append((imagePath, prediction[0]))
     cv2.waitKey(0)
 
 recognised = 0
-# see results
 for predict in predictions:
     if predict[0].split('_')[0] == predict[1].split('_')[0]:
         recognised += 1
